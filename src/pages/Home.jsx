a3c1dfd839eababa389c7209ad4318771bc03aaf
Shreyansh Vyas <shreyanshv045@gmail.com>
2024-06-25 16:13:09 +0530

    added google tracking

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,32 +1,41 @@
-import Navbar from "../Shared/Navbar"
-import Banner from "../Sections/Banner"
-import Skills from "../Sections/Skills"
-import Kpmg from "../Sections/Kpmg"
-import Faculties from "../Sections/Faculties"
-import Ramaiah from "../Sections/Ramaiah"
-import Program from "../Sections/Program"
-import Curriculum from "../Sections/Curriculum"
-import Schedule from "../Sections/Schedule"
-import Testimonial from "../Sections/Testimonial"
-import Footer from "../Shared/Footer"
-
+import Navbar from "../Shared/Navbar";
+import Banner from "../Sections/Banner";
+import Skills from "../Sections/Skills";
+import Kpmg from "../Sections/Kpmg";
+import Faculties from "../Sections/Faculties";
+import Ramaiah from "../Sections/Ramaiah";
+import Program from "../Sections/Program";
+import Curriculum from "../Sections/Curriculum";
+import Schedule from "../Sections/Schedule";
+import Testimonial from "../Sections/Testimonial";
+import Footer from "../Shared/Footer";
 
 const Home = () => {
+  useEffect(() => {
+    // Track page view
+    window.dataLayer = window.dataLayer || [];
+    function gtag() {
+      dataLayer.push(arguments);
+    }
+    gtag("js", new Date());
+    gtag("config", "AW-11454303460");
+  }, []);
+  
   return (
     <>
-    <Navbar/>
-      <Banner/>
-      <Skills/>
-      <Kpmg/>
-      <Faculties/>
-      <Ramaiah/>
-      <Program/>
-      <Curriculum/>
-      <Schedule/>
-      <Testimonial/>
-      <Footer/>
+      <Navbar />
+      <Banner />
+      <Skills />
+      <Kpmg />
+      <Faculties />
+      <Ramaiah />
+      <Program />
+      <Curriculum />
+      <Schedule />
+      <Testimonial />
+      <Footer />
     </>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
--- src/pages/Success.jsx
+++ src/pages/Success.jsx
@@ -1,54 +1,48 @@
-import React, { useEffect } from 'react'
-import Navbar from '../Shared/Navbar';
-import Button from '../Components/Button';
-import successBg from "../assets/img/ramaiah/5.jpg"
-import DownloadButton from '../Components/DownloadButton';
+import React, { useEffect } from "react";
+import Navbar from "../Shared/Navbar";
+import Button from "../Components/Button";
+import successBg from "../assets/img/ramaiah/5.jpg";
+import DownloadButton from "../Components/DownloadButton";
 
 const Success = () => {
-    
-    useEffect(() => {
-        // Track page view
-        window.dataLayer = window.dataLayer || [];
-        function gtag() {
-          dataLayer.push(arguments);
-        }
-        gtag("js", new Date());
-  
-        gtag("config", "G-MZ2ZNNH8K4");
+  useEffect(() => {
+    // Track page view
+    window.dataLayer = window.dataLayer || [];
+    function gtag() {
+      dataLayer.push(arguments);
+    }
+    gtag("js", new Date());
+    gtag("config", "AW-11454303460");
+  }, []);
 
-        window.dataLayer = window.dataLayer || [];
-        function gtag() {
-          dataLayer.push(arguments);
-        }
-        gtag("js", new Date());
-  
-        gtag("config", "AW-11454303460");
-    
-      }, []);
-    
-    return (
-        <>
-            <Navbar/>
-            <section className="bg-cover h-screen flex justify-center mt-24" style={{backgroundImage: `url(${successBg})`,
-                backgroundColor: 'rgba(0, 0, 0, 0.2)',
-                backgroundBlendMode: 'darken'}} id="home">
-                <div className='text-center backdrop-blur-md md:p-10 p-5 h-max mt-20 rounded-md md:max-w-[550px] max-w-[300px]'>
-                    <h2 className='text-white font-bold md:text-[40px] text-[20px] mb-2'>
-                        Congratulations!
-                    </h2>
-                    <p className='text-white font-garamond md:text-[25px] text-[18px] mb-8'>Registration successful. Kindly check your email for further instructions. Our counsellor will reach out to shortly.</p>
-                    
-                    <div className='flex gap-3 align-center justify-center'>  
-                     
-                    <DownloadButton
-                            text="Download Brochure"
-                            transparent={true}
-                        />
-                    </div>
-            </div>
-        </section>
-        </>
-    );
+  return (
+    <>
+      <Navbar />
+      <section
+        className="bg-cover h-screen flex justify-center mt-24"
+        style={{
+          backgroundImage: `url(${successBg})`,
+          backgroundColor: "rgba(0, 0, 0, 0.2)",
+          backgroundBlendMode: "darken",
+        }}
+        id="home"
+      >
+        <div className="text-center backdrop-blur-md md:p-10 p-5 h-max mt-20 rounded-md md:max-w-[550px] max-w-[300px]">
+          <h2 className="text-white font-bold md:text-[40px] text-[20px] mb-2">
+            Congratulations!
+          </h2>
+          <p className="text-white font-garamond md:text-[25px] text-[18px] mb-8">
+            Registration successful. Kindly check your email for further
+            instructions. Our counsellor will reach out to shortly.
+          </p>
+
+          <div className="flex gap-3 align-center justify-center">
+            <DownloadButton text="Download Brochure" transparent={true} />
+          </div>
+        </div>
+      </section>
+    </>
+  );
 };
 
-export default Success
+export default Success;
